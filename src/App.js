02968aea078563ae1711d8134ab592f2a1772bac
alexfoster1022 <alexander.f.holman@gmail.com>
2022-06-13 16:10:02 -0500

    rough draft of UI and start of functionality including key mapping and component builds

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import React, { createContext, useState } from "react";
 import "./App.css";
+import Board from "./components/Board";
+import Keyboard from "./components/Keyboard";
+import { boardDefault } from "./components/Words";
 
-function App() {
-  return <div className="App"></div>;
-}
+export const AppContext = createContext();
+
+const App = () => {
+  const [board, setBoard] = useState(boardDefault);
+  return (
+    <div className="App">
+      <header>
+        <h1>Wordle</h1>
+      </header>
+      <AppContext.Provider value={{ board, setBoard }}>
+        <div className="game">
+          <Board />
+          <Keyboard />
+        </div>
+      </AppContext.Provider>
+    </div>
+  );
+};
 
 export default App;
